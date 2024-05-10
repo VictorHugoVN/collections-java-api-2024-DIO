91a59bd01f9d4c883bd5d07b216c08071c5c968d
VictorHugoVN <victorneivadef8@gmail.com>
2024-05-10 19:26:40 -0300

    Exercício carrinho de compras adicionado

--- src/main/java/list/operacoesBasicas/CarrinhoDeCompras.java
+++ src/main/java/list/operacoesBasicas/CarrinhoDeCompras.java
@@ -1,4 +1,63 @@
 package main.java.list.operacoesBasicas;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarrinhoDeCompras {
+    List<Item> listItens;
+
+    public CarrinhoDeCompras(){
+        this.listItens = new ArrayList<>();
+    }
+
+    public void adicionarItem(String name, Double price, Integer quantity){
+        Item item = new Item(name, price, quantity);
+        listItens.add(item);
+    }
+
+    public void removerItem(String name){
+        List<Item> listItensRemover = new ArrayList<>();
+        for(Item item: listItens){
+            if(item.getName().equalsIgnoreCase(name)){
+                listItensRemover.add(item);
+            }
+        }
+        listItens.removeAll(listItensRemover);
+    }
+
+    public Double calcularValorTotal(){
+        Double total = 0.0;
+        for(Item item: listItens){
+            total += (item.getPrice() * item.getQuantity());
+        }
+        return total;
+    }
+
+    public void exibirItens(){
+        for(Item item: listItens){
+            System.out.println(item);
+        }
+    }
+
+    public static void main(String[] args){
+        CarrinhoDeCompras carrinhoDeCompras = new CarrinhoDeCompras();
+
+        carrinhoDeCompras.adicionarItem("TV", 2000.0, 3);
+        carrinhoDeCompras.adicionarItem("Smartphone", 2500.0, 2);
+        carrinhoDeCompras.adicionarItem("Computador", 4000.0, 1);
+
+        carrinhoDeCompras.exibirItens();
+
+        System.out.println(carrinhoDeCompras.calcularValorTotal());
+
+        carrinhoDeCompras.removerItem("TV");
+
+        carrinhoDeCompras.exibirItens();
+
+        System.out.println(carrinhoDeCompras.calcularValorTotal());
+
+
+    }
+
+
 }
--- src/main/java/list/operacoesBasicas/Item.java
+++ src/main/java/list/operacoesBasicas/Item.java
@@ -1,4 +1,48 @@
 package main.java.list.operacoesBasicas;
 
 public class Item {
+
+    private String name;
+    private Double price;
+    private Integer quantity;
+
+    public Item(){}
+
+    public Item(String name, Double price, Integer quantity){
+        this.setName(name);
+        this.setPrice(price);
+        this.setQuantity(quantity);
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setPrice(Double price){
+        this.price = price;
+    }
+
+    public Double getPrice(){
+        return this.price;
+    }
+
+    public void setQuantity(Integer quantity){
+        this.quantity = quantity;
+    }
+
+    public Integer getQuantity(){
+        return this.quantity;
+    }
+
+    public String toString(){
+        return this.getName() + ", R$" +
+                this.getPrice() + ", Quantidade -> " +
+                this.getQuantity();
+    }
+
+
 }
