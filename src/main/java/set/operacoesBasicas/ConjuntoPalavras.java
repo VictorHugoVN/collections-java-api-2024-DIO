72e8f0d68b17ff7a0eb5ca45ddf24059f8881f06
VictorHugoVN <victorneivadef8@gmail.com>
2024-05-11 19:24:18 -0300

    Exercício Conjunto de Palavras Únicas, adicionado

---
+++ src/main/java/set/operacoesBasicas/ConjuntoPalavras.java
@@ -0,0 +1,63 @@
+package main.java.set.operacoesBasicas;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoPalavras {
+
+    private Set<String> palavrasUnicas;
+
+    public ConjuntoPalavras(){
+        this.palavrasUnicas = new HashSet<>();
+    }
+
+    public void adicionarPalavra(String palavra){
+        this.palavrasUnicas.add(palavra);
+    }
+
+    public void removerPalavra(String palavra){
+        if(!palavrasUnicas.isEmpty()){
+            if(palavrasUnicas.contains(palavra)){
+                palavrasUnicas.remove(palavra);
+            }else{
+                System.out.println("Palavra não encontrada no conjunto!");
+            }
+        }else{
+            System.out.println("O conjunto está vazio!");
+        }
+    }
+
+    public boolean verificarPalavra(String palavra){
+        return palavrasUnicas.contains(palavra);
+    }
+
+    public void exibirPalavras(){
+        if(!palavrasUnicas.isEmpty()){
+            for(String palavra: palavrasUnicas){
+                System.out.println(palavra);
+            }
+        }else{
+            System.out.println("Conjunto vazio!");
+        }
+    }
+
+    public static void main(String[] args){
+        ConjuntoPalavras conjuntoPalavrasUnicas = new ConjuntoPalavras();
+
+        conjuntoPalavrasUnicas.adicionarPalavra("Java");
+        conjuntoPalavrasUnicas.adicionarPalavra("Python");
+        conjuntoPalavrasUnicas.adicionarPalavra("C++");
+        conjuntoPalavrasUnicas.adicionarPalavra("RSA");
+
+        System.out.println("RSA in conjunto -> " + conjuntoPalavrasUnicas.verificarPalavra("RSA"));
+        conjuntoPalavrasUnicas.exibirPalavras();
+        conjuntoPalavrasUnicas.removerPalavra("Python");
+        conjuntoPalavrasUnicas.exibirPalavras();
+
+
+    }
+
+
+
+
+}
